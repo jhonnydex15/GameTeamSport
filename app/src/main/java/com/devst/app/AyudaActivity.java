322e2f0d6f646d7695b9cf1f404a3f85e9c71f59
micha <michaelcifuentes4@gmail.com>
2025-10-17 02:04:36 -0300

    commit inicial y actualizaciones

--- app/src/main/java/com/devst/app/AyudaActivity.java
+++ app/src/main/java/com/devst/app/AyudaActivity.java
@@ -1,6 +1,10 @@
 package com.devst.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,22 @@ public class AyudaActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+
+        Button btnCorreo = findViewById(R.id.btnCorreo);
+        btnCorreo.setOnClickListener(v -> enviarCorreo());
+
+    }
+
+    private void enviarCorreo(){
+        try {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:"));
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ejemplo@instituto.com"});
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Consulta desde mi App Android");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Hola,\n\nTe escribo desde mi aplicación Android.");
+            startActivity(Intent.createChooser(emailIntent, "Enviar correo..."));
+        } catch (Exception e) {
+            Toast.makeText(this, "❌ Error al abrir cliente de correo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/devst/app/MainActivity.java
+++ app/src/main/java/com/devst/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.devst.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAyuda = findViewById(R.id.btnAyuda);
+        btnAyuda.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AyudaActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnMapa = findViewById(R.id.btnMapa);
+        btnMapa.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(intent);
+        });
+
+        Button btnFotos = findViewById(R.id.btnFotos);
+        btnFotos.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PhotoActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
