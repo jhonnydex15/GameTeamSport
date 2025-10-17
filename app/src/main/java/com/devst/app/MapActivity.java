703aaeec59e9e5a5e43acfc7f6fba8896adf969a
micha <michaelcifuentes4@gmail.com>
2025-10-17 00:54:00 -0300

    commit inicial y actualizaciones

--- app/src/main/java/com/devst/app/MapActivity.java
+++ app/src/main/java/com/devst/app/MapActivity.java
@@ -2,16 +2,12 @@ package com.devst.app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PerfilActivity extends AppCompatActivity {
-
+public class MapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_perfil);
-
+        setContentView(R.layout.activity_map);
     }
 }
